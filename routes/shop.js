f909de02c7f50ffdbf8017d41d822bbaf7b2e6a0
HrayrPetrosyan <44923866+HrayrPetrosyan@users.noreply.github.com>
2019-01-14 23:12:59 +0300

    Add files via upload

---
+++ routes/shop.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+// const adminData = require('./admin');
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct);
+// router.get('/cart', shopController.getCart);
+// router.post('/cart', shopController.postCart);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+// // router.get('/checkout', shopController.getCheckout);
+// router.get('/orders', shopController.getOrders);
+// router.post('/create-order', shopController.postOrder)
+
+
+module.exports = router;
